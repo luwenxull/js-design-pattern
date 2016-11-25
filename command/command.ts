8f7741f0cf2f4dfedbdd65eefc299492e292938f
luwenxull <992273307@qq.com>
2016-11-25 11:49:08 +0800

    more about command pattern

--- command/command.ts
+++ command/command.ts
@@ -1,3 +1,16 @@
+/*
+    命令模式的结构
+        顾名思义，命令模式就是对命令的封装，首先来看一下命令模式类图中的基本结构：
+
+        Command类：是一个抽象类，类中对需要执行的命令进行声明，一般来说要对外公布一个execute方法用来执行命令。
+        ConcreteCommand类：Command类的实现类，对抽象类中声明的方法进行实现。
+        Client类：最终的客户端调用类。
+        以上三个类的作用应该是比较好理解的，下面我们重点说一下Invoker类和Recevier类。
+
+        Invoker类：调用者，负责调用命令。
+        Receiver类：接收者，负责接收命令并且执行命令。
+*/
+
 interface Command {
     execute(): void
 }
@@ -6,9 +19,12 @@ interface Command {
     命令类
     每个命令类都有一个excute方法
 */
+
 class DanceCommand implements Command {
-    /**命令的执行者
-     * @param  {} receiver
+    /**
+     * @param {any} receiver：命令的接收者
+     * 
+     * @memberOf DanceCommand
      */
     constructor(public receiver) { }
     execute() {
@@ -36,28 +52,26 @@ let people = {
 
 interface Invoker {
     do(command: Command): void
-    [propName:string]:any
+    [propName: string]: any
 }
 
 let switcher: Invoker = {
-    commandList:[],
+    commandList: [],
     do(command) {
         this.commandList.push(command)
         command.execute()
     }
 }
 
-let dance = new DanceCommand(people),
-    jump = new JumpCommand(people);
 
-switcher.do(dance);
-switcher.do(jump);
+/*客户端逻辑*/
+function clien() {
+    let dance = new DanceCommand(people),
+        jump = new JumpCommand(people);
 
-console.log(switcher);
+        //客户端直接调用命令
+        dance.execute();
 
-/*用命令模式优化回调函数模式*/
-let btn=document.getElementById('command-btn');
-btn.onclick=function(){
-    switcher.do(dance)
+        //通过调用者调用命令
+        switcher.do(jump);
 }
-
